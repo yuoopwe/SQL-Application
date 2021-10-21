ff0e0432edf3ee12de1181bc8299398fe74da7a8
Connor Williams <connor.williams@beelivery.com>
2021-10-21 13:54:15 +0100

    Add files

---
+++ dbtry/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace dbtry
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dbData = new System.Windows.Forms.DataGridView();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.subjectTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.subjectLabel = new System.Windows.Forms.Label();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.taskIdTextBox = new System.Windows.Forms.TextBox();
+            this.taskIdLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dbData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dbData
+            // 
+            this.dbData.AllowUserToAddRows = false;
+            this.dbData.AllowUserToDeleteRows = false;
+            this.dbData.AllowUserToOrderColumns = true;
+            this.dbData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dbData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dbData.Location = new System.Drawing.Point(25, 127);
+            this.dbData.MultiSelect = false;
+            this.dbData.Name = "dbData";
+            this.dbData.ReadOnly = true;
+            this.dbData.RowTemplate.Height = 25;
+            this.dbData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dbData.Size = new System.Drawing.Size(763, 311);
+            this.dbData.TabIndex = 0;
+            this.dbData.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dbData_CellClick);
+            this.dbData.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dbData_CellContentClick);
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(252, 44);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(100, 23);
+            this.firstNameTextBox.TabIndex = 1;
+            this.firstNameTextBox.TextChanged += new System.EventHandler(this.firstNameTextBox_TextChanged);
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(427, 44);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(100, 23);
+            this.lastNameTextBox.TabIndex = 2;
+            // 
+            // subjectTextBox
+            // 
+            this.subjectTextBox.Location = new System.Drawing.Point(585, 44);
+            this.subjectTextBox.Name = "subjectTextBox";
+            this.subjectTextBox.Size = new System.Drawing.Size(100, 23);
+            this.subjectTextBox.TabIndex = 3;
+            this.subjectTextBox.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(182, 47);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(64, 15);
+            this.firstNameLabel.TabIndex = 4;
+            this.firstNameLabel.Text = "First Name";
+            this.firstNameLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(358, 47);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(63, 15);
+            this.lastNameLabel.TabIndex = 5;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // subjectLabel
+            // 
+            this.subjectLabel.AutoSize = true;
+            this.subjectLabel.Location = new System.Drawing.Point(533, 47);
+            this.subjectLabel.Name = "subjectLabel";
+            this.subjectLabel.Size = new System.Drawing.Size(46, 15);
+            this.subjectLabel.TabIndex = 6;
+            this.subjectLabel.Text = "Subject";
+            this.subjectLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(585, 76);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 6;
+            this.updateButton.Text = "Refresh";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(252, 76);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(76, 76);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(75, 23);
+            this.insertButton.TabIndex = 4;
+            this.insertButton.Text = "Insert";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(427, 76);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 7;
+            this.saveButton.Text = "Update";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // taskIdTextBox
+            // 
+            this.taskIdTextBox.AcceptsTab = true;
+            this.taskIdTextBox.Location = new System.Drawing.Point(76, 44);
+            this.taskIdTextBox.Name = "taskIdTextBox";
+            this.taskIdTextBox.Size = new System.Drawing.Size(100, 23);
+            this.taskIdTextBox.TabIndex = 8;
+            this.taskIdTextBox.TextChanged += new System.EventHandler(this.taskIdTextBox_TextChanged);
+            // 
+            // taskIdLabel
+            // 
+            this.taskIdLabel.AutoSize = true;
+            this.taskIdLabel.Location = new System.Drawing.Point(28, 50);
+            this.taskIdLabel.Name = "taskIdLabel";
+            this.taskIdLabel.Size = new System.Drawing.Size(42, 15);
+            this.taskIdLabel.TabIndex = 9;
+            this.taskIdLabel.Text = "Task Id";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.taskIdLabel);
+            this.Controls.Add(this.taskIdTextBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.subjectLabel);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.subjectTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.dbData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dbData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dbData;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox subjectTextBox;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label subjectLabel;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox taskIdTextBox;
+        private System.Windows.Forms.Label taskIdLabel;
+    }
+}
+
---
+++ dbtry/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace dbtry
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Host=beelivery-development.postgres.database.azure.com;Username=beeliveryx_dev@beelivery-development;Password=vb7VP$?yxD;Database=beeliveryx";
+        private NpgsqlConnection connection;
+        private string sqlString;
+        private NpgsqlCommand command;
+        private DataTable dataTable;
+        public int rowIndex = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new NpgsqlConnection(connectionString);
+            ConnectionLoader();
+        }
+
+        private void ConnectionLoader()
+        {
+            try
+            {
+                connection.Open();
+                sqlString = @"select * from info_select()";
+                command = new NpgsqlCommand(sqlString, connection);
+                dataTable = new DataTable();
+                dataTable.Load(command.ExecuteReader());
+                connection.Close();
+                dbData.DataSource = dataTable;
+
+            }
+            catch (Exception ex)
+            {
+                connection.Close();
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            ConnectionLoader();
+            MessageBox.Show("Refresh Successful");
+
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (rowIndex < 0)
+            {
+                MessageBox.Show("Please choose a student to delete");
+                return;
+            }
+            try
+            {
+                connection.Open();
+                sqlString = @"select * from info_delete(:_id)";
+                command = new NpgsqlCommand(sqlString, connection);
+                command.Parameters.AddWithValue("_id", int.Parse(dbData.Rows[rowIndex].Cells["_id"].Value.ToString()));
+                if ((int)command.ExecuteScalar() == 1)
+                {
+                    MessageBox.Show("Deleted student successfully");
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                connection.Close();
+                MessageBox.Show("Delete failed. Error:" + ex.Message);
+            }
+
+            rowIndex = -1;
+            taskIdTextBox.Text=firstNameTextBox.Text = subjectTextBox.Text = lastNameTextBox.Text = null;
+            ConnectionLoader();
+        }
+
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+            if (lastNameTextBox.Text != "" || taskIdTextBox.Text != "" || firstNameTextBox.Text != "" || subjectTextBox.Text != "")
+            {
+                try
+                {
+                    connection.Open();
+                    sqlString = @"insert into academy.testtable(task_id, subject, first_name, last_name) VALUES(:taskid, :value1, :firstname, :lastname)";
+                    command = new NpgsqlCommand(sqlString, connection);
+                    command.Parameters.AddWithValue("taskid", int.Parse(taskIdTextBox.Text));
+                    command.Parameters.AddWithValue("value1", subjectTextBox.Text);
+                    command.Parameters.AddWithValue("firstname", firstNameTextBox.Text);
+                    command.Parameters.AddWithValue("lastname", lastNameTextBox.Text);
+                    command.ExecuteScalar();
+                    MessageBox.Show("Student Inserted successfully");
+                    connection.Close();
+                    ConnectionLoader();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Update failed. Error: " + ex.Message);
+                    connection.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please input data for a student");
+            }
+           
+            taskIdTextBox.Text = firstNameTextBox.Text = subjectTextBox.Text = lastNameTextBox.Text = null;
+            rowIndex = -1;
+            ConnectionLoader();
+        }
+
+
+        private void UpdateButton_Click(object? sender, EventArgs e)
+        {
+            int result;
+            try
+            {
+                connection.Open();
+                sqlString = @"select * from infoo_update(:_id,:_subject,:_firstname,:_lastname)";
+                command = new NpgsqlCommand(sqlString, connection);
+                command.Parameters.AddWithValue(@"_id", int.Parse(dbData.Rows[rowIndex].Cells["_id"].Value.ToString()));
+                command.Parameters.AddWithValue(@"_firstname", firstNameTextBox.Text);
+                command.Parameters.AddWithValue(@"_lastname", lastNameTextBox.Text);
+                command.Parameters.AddWithValue(@"_subject", subjectTextBox.Text);
+                result = (int)command.ExecuteScalar();
+                connection.Close();
+                if (result == 1)
+                {
+                    MessageBox.Show("Updated successfully");
+                }
+                else
+                {
+                        MessageBox.Show("Update failed");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                    connection.Close();
+                    MessageBox.Show("Updated fail. Error: " + ex.Message);
+                    
+            }
+            taskIdTextBox.Text = firstNameTextBox.Text = subjectTextBox.Text = lastNameTextBox.Text = null;
+            rowIndex = -1;
+            ConnectionLoader();
+        }
+
+        private void dbData_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                rowIndex = e.RowIndex; 
+                firstNameTextBox.Text = dbData.Rows[e.RowIndex].Cells["_firstname"].Value.ToString();
+                subjectTextBox.Text = dbData.Rows[e.RowIndex].Cells["_subject"].Value.ToString();
+                lastNameTextBox.Text = dbData.Rows[e.RowIndex].Cells["_lastname"].Value.ToString();
+                taskIdTextBox.Text = dbData.Rows[e.RowIndex].Cells["_id"].Value.ToString();
+            }
+        }
+
+        private void firstNameTextBox_TextChanged(object sender, EventArgs e) 
+        {
+
+        }
+
+        private void taskIdTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void dbData_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+
+}
